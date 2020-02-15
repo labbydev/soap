1274035895c5268f98309000fb2903cf4611dec2
Lauren Burroughs <lburroughs@phase2technology.com>
2020-02-14 23:36:28 -0600

    get the link to each page by slug.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql, Link } from "gatsby";
 
 import Layout from "../components/layout";
 
@@ -9,6 +9,9 @@ const getMarkdownPosts = graphql`
     totalCount
     edges {
       node {
+        fields {
+          slug
+        }
         id
         frontmatter {
           title
@@ -32,7 +35,7 @@ export default () => (
                         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
                         {data.allMarkdownRemark.edges.map(({ node }) => (
                             <div key={node.id}>
-                                <h3>{node.frontmatter.title}
+                                <h3><Link to={`/posts${node.fields.slug}`}>{node.frontmatter.title}</Link>
                                     <span style={{ color:'#bbb' }}>- {node.frontmatter.date}</span>
                                 </h3>
                                 <p>{node.excerpt}</p>
