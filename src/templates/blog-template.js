6985ba7ab9f58b6b5e8cb1784c9d6fb3cfa03c18
Lauren Burroughs <lburroughs@phase2technology.com>
2020-02-15 17:14:12 -0600

    sort blog posts.

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -51,8 +51,9 @@ export default ({ data, pageContext }) => {
 export const query = graphql`
 query($skip: Int!, $limit: Int!) {
   allMarkdownRemark(
-    skip: $skip
-    limit: $limit
+    skip: $skip,
+    limit: $limit,
+    sort: { fields: [frontmatter___date], order: DESC }
   ) {
     totalCount
     edges {
