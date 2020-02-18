b444c94078f62f316804f03359db067246b04956
Lauren Burroughs <lburroughs@phase2technology.com>
2020-02-18 11:59:43 -0600

    Add links to the header.

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,12 @@ import React from "react"
 
 import gatsbyIcon from '../images/gatsby-icon.png'
 
+const isActive = ({ isCurrent }) => {
+  return { className: isCurrent ? 'active' : 'navlink' }
+}
+
+const NavLink = props => <Link getProps={isActive} {...props} />
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -20,27 +26,23 @@ const Header = ({ siteTitle }) => (
     >
         {/*Title & Logo*/}
         <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
         }}>
             <img src={gatsbyIcon} alt="gatsby logo" style={{
-                border: '3px solid orange',
-                borderRadius: '50%',
-                margin: '0 5px',
-                width: '50px',
+              border: '3px solid orange',
+              borderRadius: '50%',
+              margin: '0 5px',
+              width: '50px',
             }} />
             <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: `white`,
-                        textDecoration: `none`,
-                    }}
-                >
-                    {siteTitle}
-                </Link>
+              <NavLink to="/">
+                {siteTitle}
+              </NavLink>
             </h1>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/products">Store</NavLink>
           {/*{Shopping Cart Summary}*/}
           <div style={{ color: 'white', cursor: 'pointer' }} className="snipcart-summary snipcart-checkout">
             <div><strong>My Cart</strong></div>
